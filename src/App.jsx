7e0bc00649c7b9d7d89c72bf991c4e3df8df5448
Igor Bogdanov <i.bogdanov@ibcico.com>
2023-07-06 23:40:26 -0400

    react-tailwind boilerplate template

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 import "./css/style.css";
 
-import getFirebase from "./firebase/firebase";
-
 import { createRoot } from "react-dom/client";
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import FirebaseContext from "./auth/FirebaseContext";
-import UserContext from "./auth/UserContext";
-
 import Index from "./pages/Index";
-import SignUpPage from "./pages/SignUpPage";
-import Dashboard from "./pages/Dashboard";
-
-const fb = getFirebase();
 
 const App = () => {
-  const fireThingsHook = useState(fb);
-  const userHook = useState({});
-
   return (
     <div className="h-full">
       <BrowserRouter>
-        <FirebaseContext.Provider value={fireThingsHook}>
-          <UserContext.Provider value={userHook}>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/sign-up" element={<SignUpPage />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </UserContext.Provider>
-        </FirebaseContext.Provider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,32 +1,8 @@
-import { useState, useContext, useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
-
-import SignInForm from "./SignInForm";
-import UserContext from "../auth/UserContext";
-import FirebaseContext from "../auth/FirebaseContext";
-import { useNavigate } from "react-router-dom";
-
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
 const Index = () => {
-  const navigate = useNavigate();
-  const [fb, setFb] = useContext(FirebaseContext);
-  const [user, setUser] = useContext(UserContext);
-  const [loginStatus, setLoginStatus] = useState(true);
-
-  useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
-        console.log("already auth", user);
-        navigate(`/dashboard`);
-      } else {
-        setLoginStatus(false);
-      }
-    });
-  }, []);
-
-  return loginStatus ? null : <SignInForm />;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <h1 className="">React + Tailwind</h1>
+    </div>
+  );
 };
 export default Index;
